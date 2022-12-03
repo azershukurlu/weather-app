2238fb0d7e65031bf219a4e4b65098d6b4ca9497
azershukurlu <115929853+azershukurlu@users.noreply.github.com>
2022-12-03 21:57:18 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
+const weatherDiv = document.querySelector(".weather");
 const currentLocation = document.querySelector("#currentLocation");
 
 class App {
   constructor() {
     this.apiKey = "50653196992aeec2be1dd86c17e40203";
+    this.compassSectors = [
+      "N",
+      "NNE",
+      "NE",
+      "ENE",
+      "E",
+      "ESE",
+      "SE",
+      "SSE",
+      "S",
+      "SSW",
+      "SW",
+      "WSW",
+      "W",
+      "WNW",
+      "NW",
+      "NNW",
+      "N",
+    ];
   }
 
   setPosition(latitude, longitude) {
     this.latitude = latitude;
     this.longitude = longitude;
+  }
+
+  findWindDirection(deg) {
+    return this.compassSectors[Math.round((deg % 360) / 22.5)];
   }
 
   async fetchWeatherByLatAndLon() {
@@ -22,6 +46,80 @@ class App {
 
     return data;
   }
+
+  async fetchCountry(code) {
+    const res = await fetch(`https://restcountries.com/v3.1/alpha/${code}`);
+    const data = await res.json();
+
+    return data[0].name.common;
+  }
+
+  async show(data) {
+    const city = data.name;
+    const country = await this.fetchCountry(data.sys.country);
+    const weather = data.weather[0];
+
+    let div = `
+      <h4 class="mb-4 text-center">${city}, ${country}</h4>
+
+      <div class="d-flex justify-content-between align-items-center mb-4">
+        <div>
+          <h4>${weather.main}</h4>
+          <p>${weather.description}</p>
+        </div>
+
+        <img src="http://openweathermap.org/img/wn/${
+          weather.icon
+        }@4x.png" width="100">
+      </div>
+
+      <div class="row gy-4 mb-4">
+        <div class="col-6 col-sm-3">
+          <p class="mb-2">Real feel</p>
+          <h5>${data.main.feels_like} °C</h5>
+        </div>
+
+        <div class="col-6 col-sm-3">
+          <p class="mb-2">Temp</p>
+          <h5>${data.main.temp} °C</h5>
+        </div>
+
+        <div class="col-6 col-sm-3">
+          <p class="mb-2">Min temp</p>
+          <h5>${data.main.temp_min} °C</h5>
+        </div>
+
+        <div class="col-6 col-sm-3">
+          <p class="mb-2">Max temp</p>
+          <h5>${data.main.temp_max} °C</h5>
+        </div>
+      </div>
+
+      <div class="row gy-4">
+        <div class="col-6 col-sm-3">
+          <p class="mb-2">Humidity</p>
+          <h5>${data.main.humidity} %</h5>
+        </div>
+
+        <div class="col-6 col-sm-3">
+          <p class="mb-2">Pressure</p>
+          <h5>${data.main.pressure} hPa</h5>
+        </div>
+
+        <div class="col-6 col-sm-3">
+          <p class="mb-2">Wind speed</p>
+          <h5>${data.wind.speed} m/s</h5>
+        </div>
+
+        <div class="col-6 col-sm-3">
+          <p class="mb-2">Wind direction</p>
+          <h5>${this.findWindDirection(data.wind.deg)}</h5>
+        </div>
+      </div>
+    `;
+
+    weatherDiv.innerHTML = div;
+  }
 }
 
 const app = new App();
@@ -31,6 +129,6 @@ currentLocation.addEventListener("click", () => {
     app.setPosition(position.coords.latitude, position.coords.longitude);
     const data = await app.fetchWeatherByLatAndLon();
 
-    console.log(data);
+    app.show(data);
   });
 });
